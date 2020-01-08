44f2d8a6aff53475c47c64b58e90f2c58d2ebced
oleghbond <o.o.bondarenko@gmail.com>
2020-01-08 08:27:24 +0200

    _canonical_uri issue

---
+++ twbld.bat
@@ -0,0 +1,17 @@
+rem TESTS OF _CANONICAL_URI TIDDLER ISSUE
+rem SETLOCAL
+
+SET TW_DIR="D:\boa_protw\wiki_bag\prosteer"
+SET TMP_DIR="D:\boa_protw\_tmp\tmp_tw"
+SET IMG_FILTER="[is[image]] -[[$:/boa/logo]] -[[$:/favicon.ico]]"
+SET HTML_IMAGES_DIR="D:\boa_protw\wiki_bag\prosteer\docs\images"
+SET HTML_INDEX_FILE="D:\boa_protw\wiki_bag\prosteer\docs\index.html"
+
+xcopy /s/i/q %TW_DIR%\*.* %TMP_DIR%
+
+tiddlywiki %TMP_DIR% --savetiddlers %IMG_FILTER% %HTML_IMAGES_DIR%
+tiddlywiki %TMP_DIR% --setfield %IMG_FILTER% _canonical_uri "$:/core/templates/canonical-uri-external-image" text/plain
+tiddlywiki %TMP_DIR% --setfield %IMG_FILTER% text "" text/plain
+tiddlywiki %TMP_DIR% --rendertiddler $:/plugins/tiddlywiki/tiddlyweb/save/offline %HTML_INDEX_FILE% text/plain
+
+rem ENDLOCAL
